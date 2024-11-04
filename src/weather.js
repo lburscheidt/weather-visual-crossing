cc5283f92a5b0e4c033e585030a29b5760a32631
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-11-04 13:50:42 +0100

    feat: add icons

--- src/weather.js
+++ src/weather.js
@@ -1,18 +1,29 @@
-let tempData;
 // eslint-disable-next-line no-unused-vars
+let tempData;
+const currentIcon = document.querySelector("#current-icon");
+const currentConditions = document.querySelector("#current-conditions");
+const currentTemp = document.querySelector("#current-temp");
+const currentAlerts = document.querySelector("#current-alerts");
+const currentDescription = document.querySelector("#current-description");
+
+const dayMax = document.querySelector("#day-max");
+const currentHumidity = document.querySelector("#current-humidity");
+const currentDewpoint = document.querySelector("#current-dewpoint");
+
 export async function getWeatherData(location) {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bergen?unitGroup=us&key=DXK7PXKP245PHEUSW4KD2JYPD&contentType=json&elements=%2Baqius`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=DXK7PXKP245PHEUSW4KD2JYPD&contentType=json&elements=%2Baqius`,
     { mode: "cors" },
   );
   tempData = await response.json();
+  console.log(tempData);
   console.log(tempData.days[0].hours);
   let current = tempData.currentConditions;
   console.log(current);
   console.log("Alerts", tempData.alerts);
-  console.log("Current temperature:", current.temp);
+
   console.log("Current Feels Like", current.feelslike);
-  console.log("Current conditions", current.conditions);
+
   console.log("Current description", tempData.description);
   console.log("Current icon", current.icon);
   console.log("Current wind speed", current.windspeed);
@@ -31,5 +42,24 @@ export async function getWeatherData(location) {
   console.log("Sunrise", current.sunrise);
   console.log("Sunset", current.sunset);
   console.log("Moonphase", current.moonphase);
-  return tempData;
+  currentIcon.src = `/images/${current.icon}.svg `;
+  currentConditions.textContent = current.conditions;
+  currentTemp.textContent = current.temp + "°";
+  currentDescription.textContent = tempData.description;
+  currentHumidity.textContent = current.humidity;
+  currentDewpoint.textContent = current.dew + "°";
+  dayMax.textContent = tempData.days[0].tempmax;
+
+  if (tempData.alerts.length == 0) {
+    currentAlerts.textContent = "No weather alerts for this location";
+  } else {
+    tempData.alerts.forEach((alert) => {
+      let alertDoc = document.createElement("span");
+      alertDoc.innerHTML = `<a href=${alert.link}>${alert.headline}</a><br>${alert.description}`;
+      alertDoc.href = alert.link;
+      currentAlerts.appendChild(alertDoc);
+    });
+  }
+
+  //return tempData;
 }
