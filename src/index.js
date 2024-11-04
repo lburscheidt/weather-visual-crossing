23455ff3a4f2551382f163e3f2734c95e69ac6b0
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-11-04 11:37:52 +0100

    feat: create layout

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 import { getWeatherData } from "./weather";
-getWeatherData("Berlin");
+//getWeatherData("Berlin");
 
 //console.log(weatherData);
 
