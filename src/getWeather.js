629f61fdfba3a083169a95afd5c0d15281874273
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-11-12 11:12:26 +0100

    feat: roll weather API query functions into one

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,9 +1,86 @@
-async function getCurrentData(weatherLocation = "Berlin", unitgroup = "metric") {
+async function getWeatherData(
+	weatherLocation = "Berlin",
+	unitgroup = "metric",
+) {
 	const response = await fetch(
+		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Berlin/today/next7days/?unitGroup=metric&elements=aqius%2CdatetimeEpoch%2CresolvedAddress%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslikemax%2Cfeelslikemin%2Cfeelslike%2Cdew%2Chumidity%2Cprecip%2Cprecipprob%2Cwindspeed%2Cwindspeedmax%2Cwindspeedmin%2Cwinddir%2Cpressure%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon&include=aqius%2Chours%2Calerts%2Ccurrent&key=DXK7PXKP245PHEUSW4KD2JYPD&contentType=json`,
+		{ mode: "cors" },
+	);
+
+	const tempData = await response.json();
+	const weeklyData = tempData.days;
+	const dailyData = tempData.days[0];
+	const tomorrowData = tempData.days[1].hours;
+	const hourlyData = dailyData.hours;
+	const hourlyArray = [];
+	const tomorrowArray = [];
+	const weeklyArray = [];
+
+	for (const hour of hourlyData) {
+		const { icon, temp, datetimeEpoch, windspeed, winddir, precip } = hour;
+		hourlyArray.push({ icon, temp, datetimeEpoch, windspeed, winddir, precip });
+	}
+	for (const hour of tomorrowData) {
+		const { icon, temp, datetimeEpoch } = hour;
+		tomorrowArray.push({ icon, temp, datetimeEpoch });
+	}
+	for (const day of weeklyData) {
+		const { icon, datetimeEpoch, tempmax, tempmin } = day;
+		weeklyArray.push({ icon, datetimeEpoch, tempmax, tempmin });
+	}
+
+	const {
+		resolvedAddress,
+		alerts,
+		aqius,
+		currentConditions: { conditions, datetimeEpoch, ...rest },
+	} = tempData;
+
+	const currentWeather = {
+		resolvedAddress,
+		alerts,
+		aqius,
+		...rest,
+	};
+
+	const {
+		icon: dailyIcon,
+		tempmax: dailyTempMax,
+		tempmin: dailyTempMin,
+	} = dailyData;
+
+	const dailyWeather = {
+		icon: dailyIcon,
+		tempmax: dailyTempMax,
+		tempmin: dailyTempMin,
+	};
+
+	const weatherObject = [
+		currentWeather,
+		dailyWeather,
+		hourlyArray,
+		tomorrowArray,
+		weeklyArray,
+	];
+	console.log(weatherObject);
+	return weatherObject;
+}
+
+export { getWeatherData };
+
+/*async function getCurrentData(
+	weatherLocation = "Berlin",
+	unitgroup = "metric",
+) {
+	const response = await fetch(
+
+
+
 		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${weatherLocation}/today?unitGroup=${unitgroup}&include=alerts%2Ccurrent&elements=aqius%2CdatetimeEpoch%2Cdescription%2CresolvedAddress%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslikemax%2Cfeelslikemin%2Cfeelslike%2Cdew%2Chumidity%2Cprecip%2Cprecipprob%2Cwindspeedmax%2Cwindspeedmean%2Cwindspeedmin%2Cwindspeed%2Cwinddir%2Cpressure%2Cvisibility%2Cuvindex%2CsunriseEpoch%2CsunsetEpoch%2Cmoonphase%2Cconditions%2Cdescription%2Cicon&key=DXK7PXKP245PHEUSW4KD2JYPD&contentType=json`,
 		{ mode: "cors" },
 	);
 	const tempData = await response.json();
+	console.log(tempData);
 	const currentData = tempData.currentConditions;
 	const dailyData = tempData.days[0];
 
@@ -73,7 +150,10 @@ async function getHourlyData(weatherLocation = "Berlin", unitgroup = "metric") {
 	return hourlyWeather;
 }
 
-async function getTomorrowData(weatherLocation = "Berlin", unitgroup = "metric") {
+async function getTomorrowData(
+	weatherLocation = "Berlin",
+	unitgroup = "metric",
+) {
 	const response = await fetch(`
 https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${weatherLocation}/tomorrow?unitGroup=${unitgroup}&include=hours%2Ccurrent&elements=datetimeEpoch%2Ctemp%2Cicon&key=DXK7PXKP245PHEUSW4KD2JYPD&contentType=json`);
 	const tempData = await response.json();
@@ -104,4 +184,4 @@ async function getWeeklyData(weatherLocation = "Berlin", unitgroup = "metric") {
 	return week;
 }
 
-export { getCurrentData, getHourlyData, getTomorrowData, getWeeklyData };
+export { getCurrentData, getHourlyData, getTomorrowData, getWeeklyData };*/
